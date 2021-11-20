012f8cc026baaa1451b84c4c7bc19319fe6baa53
mbp2019 <osakaihsigaki@gmail.com>
2021-11-20 18:11:51 +0900

    routes controllers html でリンク繋げた

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
     root "top#index" 
+
+    resources :articles, only: [:show, :new]
+    resources :users, only: [:show, :new]
+    resources :sessions, only: [:new]
 end
